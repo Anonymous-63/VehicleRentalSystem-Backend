f968de8015df740cf317f486fbfd28828e5705c4
anonymous-63 <anonymous63.krutik@gmail.com>
2025-01-23 22:29:24 +0530

    User Create controller service

---
+++ src/main/java/com/anonymous63/vrs/models/dtos/reqDtos/UserReqDto.java
@@ -0,0 +1,19 @@
+package com.anonymous63.vrs.models.dtos.reqDtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserReqDto {
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String password;
+    private String about;
+    private Boolean enabled;
+}
---
+++ src/main/java/com/anonymous63/vrs/models/dtos/resDtos/UserResDto.java
@@ -0,0 +1,11 @@
+package com.anonymous63.vrs.models.dtos.resDtos;
+
+import lombok.Data;
+
+@Data
+public class UserResDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String about;
+}
